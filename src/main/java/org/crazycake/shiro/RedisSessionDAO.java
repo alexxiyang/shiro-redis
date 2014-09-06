897db4f206f8793e33a7fddfd2d2112b8eca555d
jetgeng <jetgeng@gmail.com>
2014-09-07 07:25:56 +0800

    使用guava加上短时间的内存缓存

--- src/main/java/org/crazycake/shiro/RedisSessionDAO.java
+++ src/main/java/org/crazycake/shiro/RedisSessionDAO.java
@@ -4,7 +4,12 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.session.mgt.eis.AbstractSessionDAO;
@@ -19,86 +24,129 @@ public class RedisSessionDAO extends AbstractSessionDAO {
 	 * shiro-redis的session对象前缀
 	 */
 	private RedisManager redisManager;
-	
+
+	private LoadingCache<Serializable, Session> sessionCache = CacheBuilder.newBuilder().maximumSize(1024).expireAfterWrite(3, TimeUnit.MINUTES).build(
+			new CacheLoader<Serializable, Session>() {
+				public Session load(Serializable key) {
+					long start = System.currentTimeMillis();
+					Session s = (Session) SerializeUtils.deserialize(redisManager.get(getByteKey(key)));
+					logger.info("get session from redis" + key.toString() + " token time is:" + (System.currentTimeMillis() - start));
+					return s;
+				}
+			});
+
+
 	/**
-	 * The Redis key prefix for the sessions 
+	 * The Redis key prefix for the sessions
 	 */
 	private String keyPrefix = "shiro_redis_session:";
-	
+
+	@Override
+	protected Serializable doCreate(Session session) {
+		long start = System.currentTimeMillis();
+		Serializable sessionId = generateSessionId(session);
+		this.assignSessionId(session, sessionId);
+
+		this.saveSession(session);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Create session :" + (System.currentTimeMillis() - start));
+		}
+		return sessionId;
+	}
+
+
 	@Override
 	public void update(Session session) throws UnknownSessionException {
+		long start = System.currentTimeMillis();
 		this.saveSession(session);
+		if (logger.isDebugEnabled()) {
+			logger.debug("update session :" + (System.currentTimeMillis() - start));
+		}
 	}
-	
+
 	/**
 	 * save session
+	 *
 	 * @param session
 	 * @throws UnknownSessionException
 	 */
-	private void saveSession(Session session) throws UnknownSessionException{
-		if(session == null || session.getId() == null){
+	private void saveSession(Session session) throws UnknownSessionException {
+		long start = System.currentTimeMillis();
+		if (session == null || session.getId() == null) {
 			logger.error("session or session id is null");
 			return;
 		}
-		
+
 		String key = getByteKey(session.getId());
 		byte[] value = SerializeUtils.serialize(session);
-		session.setTimeout(redisManager.getExpire()*1000);		
+		session.setTimeout(redisManager.getExpire() * 1000);
 		this.redisManager.set(key, value, redisManager.getExpire());
+		if (logger.isDebugEnabled()) {
+			logger.debug("saveSession :" + (System.currentTimeMillis() - start));
+		}
 	}
 
 	@Override
 	public void delete(Session session) {
-		if(session == null || session.getId() == null){
+		long start = System.currentTimeMillis();
+		if (session == null || session.getId() == null) {
 			logger.error("session or session id is null");
 			return;
 		}
 		redisManager.del(this.getByteKey(session.getId()));
-
+		if (logger.isDebugEnabled()) {
+			logger.debug("delete :" + (System.currentTimeMillis() - start));
+		}
 	}
 
 	@Override
 	public Collection<Session> getActiveSessions() {
+		long start = System.currentTimeMillis();
 		Set<Session> sessions = new HashSet<Session>();
-		
+
 		Set<byte[]> keys = redisManager.keys(this.keyPrefix + "*");
-		if(keys != null && keys.size()>0){
-			for(byte[] key:keys){
-				Session s = (Session)SerializeUtils.deserialize(redisManager.get(SafeEncoder.encode(key)));
+		if (keys != null && keys.size() > 0) {
+			for (byte[] key : keys) {
+				Session s = (Session) SerializeUtils.deserialize(redisManager.get(SafeEncoder.encode(key)));
 				sessions.add(s);
 			}
 		}
-		
+		if (logger.isDebugEnabled()) {
+			logger.debug("getActiveSessions :" + (System.currentTimeMillis() - start));
+		}
 		return sessions;
 	}
 
-	@Override
-	protected Serializable doCreate(Session session) {
-		Serializable sessionId = this.generateSessionId(session);  
-        this.assignSessionId(session, sessionId);
-        this.saveSession(session);
-		return sessionId;
-	}
 
 	@Override
 	protected Session doReadSession(Serializable sessionId) {
-		if(sessionId == null){
+		long start = System.currentTimeMillis();
+		if (sessionId == null) {
 			logger.error("session id is null");
 			return null;
 		}
-		
-		Session s = (Session)SerializeUtils.deserialize(redisManager.get(this.getByteKey(sessionId)));
+
+		Session s = null; // (Session) SerializeUtils.deserialize(redisManager.get(this.getByteKey(sessionId)));
+		try {
+			s = sessionCache.get(sessionId);
+			if (logger.isDebugEnabled()) {
+				logger.debug("doReadSession session id:" + sessionId.toString() + " token time is:" + (System.currentTimeMillis() - start));
+			}
+		} catch (ExecutionException e) {
+			logger.error(e.getMessage(), e);
+			return s;
+		}
 		return s;
 	}
-	
+
 	/**
 	 * 获得byte[]型的key
-	 * @param key
+	 *
+	 * @param sessionId
 	 * @return
 	 */
-	private String getByteKey(Serializable sessionId){
-		String preKey = this.keyPrefix + sessionId;
-		return preKey;
+	private String getByteKey(Serializable sessionId) {
+		return this.keyPrefix + sessionId;
 	}
 
 	public RedisManager getRedisManager() {
@@ -107,7 +155,7 @@ public class RedisSessionDAO extends AbstractSessionDAO {
 
 	public void setRedisManager(RedisManager redisManager) {
 		this.redisManager = redisManager;
-		
+
 		/**
 		 * 初始化redisManager
 		 */
@@ -117,6 +165,7 @@ public class RedisSessionDAO extends AbstractSessionDAO {
 	/**
 	 * Returns the Redis session keys
 	 * prefix.
+	 *
 	 * @return The prefix
 	 */
 	public String getKeyPrefix() {
@@ -124,13 +173,14 @@ public class RedisSessionDAO extends AbstractSessionDAO {
 	}
 
 	/**
-	 * Sets the Redis sessions key 
+	 * Sets the Redis sessions key
 	 * prefix.
+	 *
 	 * @param keyPrefix The prefix
 	 */
 	public void setKeyPrefix(String keyPrefix) {
 		this.keyPrefix = keyPrefix;
 	}
-	
-	
+
+
 }
--- src/main/java/org/crazycake/shiro/SerializeUtils.java
+++ src/main/java/org/crazycake/shiro/SerializeUtils.java
@@ -10,18 +10,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SerializeUtils {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(SerializeUtils.class);
-	
+
 	/**
 	 * 反序列化
+	 *
 	 * @param bytes
 	 * @return
 	 */
 	public static Object deserialize(byte[] bytes) {
-		
+		long start = System.currentTimeMillis();
 		Object result = null;
-		
+
 		if (isEmpty(bytes)) {
 			return null;
 		}
@@ -32,39 +33,41 @@ public class SerializeUtils {
 				ObjectInputStream objectInputStream = new ObjectInputStream(byteStream);
 				try {
 					result = objectInputStream.readObject();
-				}
-				catch (ClassNotFoundException ex) {
+				} catch (ClassNotFoundException ex) {
 					throw new Exception("Failed to deserialize object type", ex);
 				}
-			}
-			catch (Throwable ex) {
+			} catch (Throwable ex) {
 				throw new Exception("Failed to deserialize", ex);
 			}
 		} catch (Exception e) {
-			logger.error("Failed to deserialize",e);
+			logger.error("Failed to deserialize", e);
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("deserialize object time is:" + (System.currentTimeMillis() - start));
 		}
 		return result;
 	}
-	
+
 	public static boolean isEmpty(byte[] data) {
 		return (data == null || data.length == 0);
 	}
 
 	/**
 	 * 序列化
+	 *
 	 * @param object
 	 * @return
 	 */
 	public static byte[] serialize(Object object) {
-		
+		long start = System.currentTimeMillis();
 		byte[] result = null;
-		
+
 		if (object == null) {
 			return new byte[0];
 		}
 		try {
 			ByteArrayOutputStream byteStream = new ByteArrayOutputStream(128);
-			try  {
+			try {
 				if (!(object instanceof Serializable)) {
 					throw new IllegalArgumentException(SerializeUtils.class.getSimpleName() + " requires a Serializable payload " +
 							"but received an object of type [" + object.getClass().getName() + "]");
@@ -72,13 +75,15 @@ public class SerializeUtils {
 				ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteStream);
 				objectOutputStream.writeObject(object);
 				objectOutputStream.flush();
-				result =  byteStream.toByteArray();
-			}
-			catch (Throwable ex) {
+				result = byteStream.toByteArray();
+			} catch (Throwable ex) {
 				throw new Exception("Failed to serialize", ex);
 			}
 		} catch (Exception ex) {
-			logger.error("Failed to serialize",ex);
+			logger.error("Failed to serialize", ex);
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("serialize object time is:" + (System.currentTimeMillis() - start));
 		}
 		return result;
 	}
